b9979625d69c4b2c8af6173e1fc6b9492e713158
Quinten van Breugel <quintenvanbreugel@wlan087046.mobiel.utwente.nl>
2019-09-25 09:17:21 +0200

    Cookie Banner + Development Banner Added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Introduction from "./components/sections/introduction/Introduction";
 import Eggs from "./components/sections/eggs/Eggs";
 import Pigs from "./components/sections/pigs/Pigs";
 import Contact from "./components/sections/contact/Contact";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
     <div>
       <Header />
-
       <Introduction />
       <Eggs />
       <Pigs inStock />
       <Contact />
+      <CookieConsent
+        buttonText="Ik begrijpt het"
+        style={{ background: "#bababa", color: "#333333" }}
+        buttonStyle={{
+          background: "#e6e6e6",
+          color: "#333333"
+        }}
+      >
+        Deze website gebruikt cookies om de gebruikerservaring te verbeteren. Op
+        dit moment is de website nog in de ontwikkelfase en komt niet overeen
+        met het eindresultaat.
+      </CookieConsent>
     </div>
   );
 }
